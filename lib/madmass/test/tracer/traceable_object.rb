49c39c3f44b8851f572d161478ec4fe8cf3909b4
Algorithmica <info@algorithmica.it>
2011-08-31 17:29:41 +0200

    fixed some comment

--- lib/madmass/test/tracer/traceable_object.rb
+++ lib/madmass/test/tracer/traceable_object.rb
@@ -1,6 +1,6 @@
 # This class is used to test traceability on simple ruby objects.
 class TraceableObject
-  # you can also skip attr1 and attr2 accessor definition, the tracer will do it for us
+  # you can also skip attr1 and attr2 accessor definition, the tracer will do it for you
   attr_accessor :attr1, :attr2, :attr3
   trace :attr1, :attr2
 end
