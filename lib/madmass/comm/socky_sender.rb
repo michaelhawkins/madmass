d385aa8ac58f74f5f0273e269e4f18452518e497
ziparo <ziparo@algorithmica.it>
2011-09-30 02:03:12 +0200

    Added SockySender
    An adapter for using Socky as WebSocket client

---
+++ lib/madmass/comm/socky_sender.rb
@@ -0,0 +1,17 @@
+module Madmass
+  module Comm
+          
+    class SockySender
+      include Singleton
+
+      class << self
+        def send(js, options = {})
+          Socky.send(js.html_safe, options)
+        end
+      end
+      
+    end
+
+  end
+end
+
