c764538fc8bea57044379818d6d458b62ffffc8c
ziparo <ziparo@algorithmica.it>
2011-10-01 22:49:51 +0200

    Added generation of tests in the action generator

--- lib/generators/action/action_generator.rb
+++ lib/generators/action/action_generator.rb
@@ -1,12 +1,15 @@
+
 class ActionGenerator < Rails::Generators::NamedBase
 
   desc "This generator creates an action file at lib/actions"
 
   source_root File.expand_path("../templates", __FILE__)
 
-  #Adds in the action directory a "file_name action" rb class template
-  def render_action_file
+  #Adds in the lib/action directory a "file_name action" rb class template
+  #Adds in the test/unit directory  a "file_name action" unit test template
+  def render_action_files
     template "action.rb.erb", "lib/actions/#{file_name}_action.rb"
+    template "action_unit_test.rb.erb", "test/unit/#{file_name}_action_test.rb"
   end
 
 end
