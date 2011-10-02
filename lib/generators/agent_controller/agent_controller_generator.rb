69d828b3b9a051a07f07cfca5b07749a33bff146
ziparo <ziparo@algorithmica.it>
2011-10-02 16:34:40 +0200

    A first agent controller generator
    Creates a controller for interacting with the agent that responds both
    to html and json

---
+++ lib/generators/agent_controller/agent_controller_generator.rb
@@ -0,0 +1,15 @@
+class AgentControllerGenerator < Rails::Generators::NamedBase
+
+  desc "This generator creates ajax and html controllers, for linking the user to the agent"
+
+  source_root File.expand_path("../templates", __FILE__)
+
+  #Adds in the lib/action directory a "file_name action" rb class template
+  #Adds in the test/unit directory  a "file_name action" unit test template
+  def render_action_files
+    raise "The agent controller name can not be blank!" if file_name.blank?
+    template "agent_controller.rb.erb", "app/controllers/#{file_name}_agent_controller.rb"
+    route(" match '#{file_name}', :to => '#{file_name}#execute'")
+  end
+
+end
