0ce7eae992f0637e831a3a114cfd1922b4e83cd6
ziparo <ziparo@algorithmica.it>
2011-10-22 13:35:48 +0200

    removed agent.rb

--- lib/agent.rb
+++
@@ -1,9 +0,0 @@
-# Load the Agent module
-
-# Load the agent modules.
-agent_path = File.join(Madmass.root, 'madmass', 'agent')
-require File.join(agent_path, 'executor')
-require File.join(agent_path, 'f_s_a_executor')
-require File.join(agent_path, 'current')
-require File.join(agent_path, 'proxy_agent')
-require File.join(agent_path, 'jms_executor') if defined? TorqueBox
