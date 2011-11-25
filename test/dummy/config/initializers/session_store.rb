793d92f6c359265b5791b47d36c648f7c3c47280
genzoo <cottefoglie@algorithmica.it>
2011-11-26 00:16:58 +0100

    removed torquebox from dummy test rails app

--- test/dummy/config/initializers/session_store.rb
+++ test/dummy/config/initializers/session_store.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Be sure to restart your server when you modify this file.
 
 Dummy::Application.config.session_store :cookie_store, :key => '_dummy_session'
@@ -7,8 +6,3 @@ Dummy::Application.config.session_store :cookie_store, :key => '_dummy_session'
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # Dummy::Application.config.session_store :active_record_store
-=======
-# Configure the TorqueBox Servlet-based session store.
-# Provides for server-based, in-memory, cluster-compatible sessions
-Dummy::Application.config.session_store :torquebox_store
->>>>>>> c0c42f90b990a880f3b144a0a00188e4c105632d
