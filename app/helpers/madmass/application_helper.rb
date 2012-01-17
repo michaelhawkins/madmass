10e0940cbc87e0a5f800d7f4dbd49450ba2e7de3
genzoo <cottefoglie@algorithmica.it>
2012-01-17 17:38:34 +0100

    load socky on window load, resolve initialization issues

--- app/helpers/madmass/application_helper.rb
+++ app/helpers/madmass/application_helper.rb
@@ -17,12 +17,25 @@ module Madmass
       ws_client = Madmass.install_options(:ws_client)
       case ws_client
       when :socky
-        socky(:channels => options[:channels], :client_id => options[:client])
+        madsocky(:channels => options[:channels], :client_id => options[:client])
       when :stilts
         stilts(:channels => options[:channels], :client_id => options[:client])
       else
         raise Madmass::Errors::WrongInputError, "Cannot find the web socket client library: #{ws_client}"
       end
+    end
+
+    def madsocky(options)
+      host = Socky.random_host
+      options = {
+        :host                 => (host[:secure] ? "wss://" : "ws://") + host[:host],
+        :port                 => host[:port],
+      }.merge(options)
+      javascript_tag <<SOCKY_JS
+        $(window).load(function() {
+          new Socky('#{options.delete(:host)}', '#{options.delete(:port)}', '#{options.to_query}');
+        });
+SOCKY_JS
     end
 
     def stilts(options)
@@ -31,12 +44,12 @@ module Madmass
         :port => Madmass.install_options(:stomplet_port),
         :topic => Madmass.install_options(:stomplet_topic)
       }
-      javascript_tag <<JS
+      javascript_tag <<STILTS_JS
         $(window).load(function() {
           var stiltsClient = Madmass.Stomp.getInstance(#{connection_options.to_json});
           stiltsClient.register();
         });
-JS
+STILTS_JS
     end
 
   end
