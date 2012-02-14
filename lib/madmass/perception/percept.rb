73118ddcb3fe6923c5b2d6bf1ad571633d9c58da
genzoo <cottefoglie@algorithmica.it>
2012-02-14 19:51:37 +0100

    added license to all file

--- lib/madmass/perception/percept.rb
+++ lib/madmass/perception/percept.rb
@@ -1,3 +1,32 @@
+###############################################################################
+###############################################################################
+#
+# This file is part of MADMASS (MAssively Distributed Multi Agent System Simulator).
+#
+# Copyright (c) 2012 Algorithmica Srl
+#
+# MADMASS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MADMASS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with MADMASS.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact us via email at info@algorithmica.it or at
+#
+# Algorithmica Srl
+# Vicolo di Sant'Agata 16
+# 00153 Rome, Italy
+#
+###############################################################################
+###############################################################################
+
 # This class represents a single Percept generated as result of, possibly part of,
 # an action. The current perception is an array of such percepts. Each percept
 # is composed of three Hashes: the header, the data and the status.
