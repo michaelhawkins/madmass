766568b9c2dee65f710eef474c5fb63423b4c209
ziparo <ziparo@algorithmica.it>
2012-04-10 11:30:44 +0200

    Updated to TB2.0.0

--- lib/madmass/agent/executor.rb
+++ lib/madmass/agent/executor.rb
@@ -32,9 +32,9 @@ module Madmass
   module Agent
     module Executor
       include Madmass::Transaction::TxMonitor
-     
+
       def execute(usr_opts = {})
-                
+
         #prepare opts
         opts = usr_opts.clone
         opts[:agent] = self
@@ -50,7 +50,7 @@ module Madmass
         Madmass.dispatch_percepts
 
         #return the I18n perception
-        return  status
+        return status
       end
 
 
@@ -79,7 +79,7 @@ module Madmass
             process(action)
           end
         end
-          
+
         return 'ok' #http status
 
       rescue Madmass::Errors::StateMismatchError => exc
@@ -87,79 +87,81 @@ module Madmass
 
       rescue Madmass::Errors::NotApplicableError => exc
         error_percept_factory(action, exc,
-          :code => 'precondition_failed',
-          :why_not_applicable => action.why_not_applicable.as_json)
+                              :code => 'precondition_failed',
+                              :why_not_applicable => action.why_not_applicable.as_json)
         return :precondition_failed #http status
 
       rescue Madmass::Errors::WrongInputError => exc
         error_percept_factory(action, exc,
-          :code => 'bad_request',
-          :message => exc.message)
+                              :code => 'bad_request',
+                              :message => exc.message)
         return :bad_request #http status
 
       rescue Madmass::Errors::CatastrophicError => exc
         error_percept_factory(action, exc,
-          :code => 'internal_server_error',
-          :message => exc.message)
+                              :code => 'internal_server_error',
+                              :message => exc.message)
         return :internal_server_error #http status
-        
+
       rescue Exception => exc
         error_percept_factory(action, exc,
-          :code => 'service_unavailable',
-          :message => exc.message)
+                              :code => 'service_unavailable',
+                              :message => exc.message)
         return :service_unavailable #http status
       end
-
-      private
-
-      def process(action)
-        # check if the action is consistent with behavioral specification
-        # e.g., FSA, PNP, etc ...
-        behavioral_validation(action)
-
-        # check action specific applicability
-        unless action.applicable?
-          raise Madmass::Errors::NotApplicableError
-        end
-
-        # execute action
-        action.execute
-
-        # change user state
-        action.change_state
-
-        # generate percept (in Madmass.current_percept)
-        action.build_result
-      end
-      
-      def create_action(opts)
-        # when the remote option is passed other options are translated to create a remote action
-        if opts.delete(:remote)
-          data = opts.clone
-          cmd = "madmass::action::remote"
-          opts = {:cmd => cmd, :data => data}
-        end
-        Madmass::Action::ActionFactory.make(opts)
-      end
-      
-      def error_percept_factory(action, error, opts)
-
-        error_msg = "#{action} #{error.class}: #{error.message}"
-        error_msg += " - #{action.why_not_applicable.messages}" if action and action.why_not_applicable.any?
-        Madmass.logger.error(error_msg)
-
-        e = Madmass::Perception::Percept.new(action)
-        e.status = {:code => opts[:code], :exception => error.class.name}
-        e.data.merge!({:message => opts[:message]}) if opts[:message]
-        e.data.merge!({:why_not_applicable => opts[:why_not_applicable]}) if opts[:why_not_applicable]
-
-        Madmass.current_perception << e
-      end
-
-      def behavioral_validation action
-        return true;
-      end
- 
     end
   end
+
+  private
+
+  def process(action)
+    # check if the action is consistent with behavioral specification
+    # e.g., FSA, PNP, etc ...
+    behavioral_validation(action)
+
+    # check action specific applicability
+    unless action.applicable?
+      raise Madmass::Errors::NotApplicableError
+    end
+
+    # execute action
+    action.execute
+
+    # change user state
+    action.change_state
+
+    # generate percept (in Madmass.current_percept)
+    action.build_result
+  end
+
+  def create_action(opts)
+    # when the remote option is passed other options are translated to create a remote action
+    if opts.delete(:remote)
+      data = opts.clone
+      cmd = "madmass::action::remote"
+      opts = {:cmd => cmd, :data => data}
+    end
+    Madmass::Action::ActionFactory.make(opts)
+  end
+
+  def error_percept_factory(action, error, opts)
+
+    error_msg = "#{action} #{error.class}: #{error.message}"
+    error_msg += " - #{action.why_not_applicable.messages}" if action and action.why_not_applicable.any?
+    Madmass.logger.error(error_msg)
+
+    e = Madmass::Perception::Percept.new(action)
+    e.status = {:code => opts[:code], :exception => error.class.name}
+    e.data.merge!({:message => opts[:message]}) if opts[:message]
+    e.data.merge!({:why_not_applicable => opts[:why_not_applicable]}) if opts[:why_not_applicable]
+
+    Madmass.current_perception << e
+  end
+
+  def behavioral_validation action
+    return true;
+  end
+
+end
+end
 end
--- madmass.gemspec
+++ madmass.gemspec
@@ -27,12 +27,12 @@ Transaction Processing Systems, Location-based Mobile Social Networks (or geo-so
   s.add_dependency "shoulda", ">= 0"
   s.add_dependency "bundler"
   s.add_dependency "rcov", "~> 0.9.11"
-  s.add_dependency "torquebox-rake-support", "2.0.0.beta3"
-  s.add_dependency "torquebox", "2.0.0.beta3"
+  s.add_dependency "torquebox-rake-support", "2.0.0"
+  s.add_dependency "torquebox", "2.0.0"
   # FIXME: uncomment when agent farm will be published and remove from Gemfile of client apps
   #s.add_dependency "agent_farm"
 
   #s.add_dependency 'i18n'
   #s.add_dependency "activesupport"
-  s.add_dependency "rails", "3.1.3"
+  s.add_dependency "rails"
 end
